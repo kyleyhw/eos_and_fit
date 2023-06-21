e335de3cb80dab1ce716d5e5c847803e3f25aa81
Kyle <kyleyhw@gmail.com>
2023-06-21 14:47:25 -0400

    strange artefacts in plots

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import matplotlib.pyplot as plt
 from data_loader import DataLoader
 from eos import EOS
 
-data = DataLoader('data/macro-alf2cr.csv')
-alf2cr = EOS(data, name='alf2cr', q=1)
-
 fig, ax = plt.subplots(1, 1)
-alf2cr.plot(ax=ax)
+
+names = ['alf2cr', 'alf4cr', 'bsk20cr']
+qs = [0.5, 0.75, 0.9]
+for name in names:
+    for q in qs:
+        data = DataLoader('data/macro-' + name + '.csv')
+        eos = EOS(data, name=name, q=q)
+        eos.plot(ax=ax)
+
+ax.set_ylim((10, 10000))
+ax.set_xlim((10, 10000))
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+ax.set_xlabel(r'\Lambda_s')
+ax.set_ylabel(r'\Lambda_a')
+plt.legend()
 
 plt.savefig('plot.png')
