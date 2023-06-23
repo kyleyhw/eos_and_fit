951038a0200cc178becb76efde4d3b2cd6152987
kyleyhw <kyleyhw@gmail.com>
2023-06-23 16:45:53 -0400

    removed redundant imports

--- eos.py
+++ eos.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
 
 from data_loader import DataLoader
 
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 from fits import YagiYunes
 from plotter import Plotter
 
